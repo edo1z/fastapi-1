84e5bdbe8da54b1d5402614bf5372a802801f763
edo1z <endo.yuta@logicky.com>
2024-12-13 22:16:23 +0900

    ChatPromptTemplateを使ってみた

--- app/chat.py
+++ app/chat.py
@@ -1,21 +1,26 @@
 from langchain_core.messages import HumanMessage, SystemMessage
 from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
 from .config import OPENAI_API_KEY
 from typing import AsyncGenerator
 
 
 async def get_chat_stream(user_message: str) -> AsyncGenerator[str, None]:
     model = ChatOpenAI(
-        model="gpt-3.5-turbo",
-        openai_api_key=OPENAI_API_KEY,
-        streaming=True
+        model="gpt-3.5-turbo", openai_api_key=OPENAI_API_KEY, streaming=True
     )
 
-    messages = [
-        SystemMessage(content="日本語から英語に翻訳してください。"),
-        HumanMessage(content=user_message),
-    ]
+    prompt_template = ChatPromptTemplate.from_messages(
+        [
+            ("system", "Translate the following from {source_lang} into {target_lang}"),
+            ("user", "{text}"),
+        ]
+    )
 
-    async for chunk in model.astream(messages):
+    prompt = prompt_template.invoke(
+        {"source_lang": "Japanese", "target_lang": "English", "text": user_message}
+    )
+
+    async for chunk in model.astream(prompt):
         if chunk.content:
             yield chunk.content
