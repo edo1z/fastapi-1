d186bbaea2ebb00559199c42effb727153f7c3da
edo1z <endo.yuta@logicky.com>
2024-12-14 11:23:34 +0900

    数値ベクトル化

--- app/search.py
+++ app/search.py
@@ -1,5 +1,7 @@
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+from .config import OPENAI_API_KEY
 import os
 
 
@@ -15,6 +17,16 @@ def count_pages():
         chunk_size=1000, chunk_overlap=200, add_start_index=True
     )
     all_splits = text_splitter.split_documents(docs)
-    print(all_splits[0])
 
-    return len(all_splits)
+    embeddings = OpenAIEmbeddings(
+        model="text-embedding-3-large", openai_api_key=OPENAI_API_KEY
+    )
+
+    vector_1 = embeddings.embed_query(all_splits[0].page_content)
+    vector_2 = embeddings.embed_query(all_splits[1].page_content)
+
+    assert len(vector_1) == len(vector_2)
+    print(f"Generated vectors of length {len(vector_1)}\n")
+    print(vector_1[:10])
+
+    return len(vector_1)
